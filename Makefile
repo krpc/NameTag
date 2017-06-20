8f0dad3b57cd64101732ea2e374f02196c34b1b4
djungelorm <djungelorm@users.noreply.github.com>
2017-06-20 17:44:59 +0100

    Add KSP_VERSION file and update ksp-avc script to use it

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all:
 	msbuild /warnaserror /nowarn:MSB3276,MSB3277 /p:Configuration=Release NameTag.sln
 	mkdir -p GameData/NameTag
-	python tools/ksp-avc.py `tools/version.sh` > GameData/NameTag/NameTag.version
+	python tools/ksp-avc.py `tools/version.sh` `tools/ksp-version.sh` > GameData/NameTag/NameTag.version
 	cp LICENSE.md CHANGELOG.md src/bin/Release/NameTag.dll src/module-manager.cfg GameData/NameTag/
 	zip -r NameTag-`tools/version.sh`.zip GameData
 
--- tools/ksp-avc.py
+++ tools/ksp-avc.py
@@ -7,8 +7,9 @@ content = '{\n' + \
 '  "URL": "http://ksp-avc.cybutek.net/version.php?id=344",\n' + \
 '  "DOWNLOAD": "https://github.com/krpc/NameTag/releases/latest",\n' + \
 '  "VERSION": { "MAJOR": %d, "MINOR": %d, "PATCH": %d },\n' + \
-'  "KSP_VERSION": { "MAJOR": 1, "MINOR": 1, "PATCH": 3 }\n' + \
+'  "KSP_VERSION": { "MAJOR": %d, "MINOR": %d, "PATCH": %d }\n' + \
 '}'
 
 version_parts = sys.argv[1].split('.')
-print(content % tuple(int(x) for x in version_parts))
+ksp_version_parts = sys.argv[2].split('.')
+print(content % tuple(int(x) for x in version_parts + ksp_version_parts))
